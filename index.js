575fdf043ac441a575c4243acaf7b4e35cafe19a
gani2009 <gani.omyrzak@gmail.com>
2024-05-08 17:13:41 -0400

    added stats

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.use(cors());
 var expressWs = require('express-ws')(app);
 const clientsByURL = {};
 
+let serverStartDate = new Date();
+let stats = {
+  websocket: {
+    totalClients: 0,
+    totalMessages: 0
+  },
+  apiLyrics: {
+    numRequests: 0,
+    num404requests: 0
+  }
+};
+
 // Define WebSocket route
 app.ws('/websocket/:url', (ws, req) => {
   const urlParam = req.params.url;
@@ -18,6 +30,7 @@ app.ws('/websocket/:url', (ws, req) => {
     clientsByURL[urlParam] = [];
   };
   clientsByURL[urlParam].push(ws);
+  stats.websocket.totalClients++;
 
 
   ws.on('message', (msg) => {
@@ -26,6 +39,7 @@ app.ws('/websocket/:url', (ws, req) => {
       clientsByURL[urlParam].forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(msg);
+          stats.websocket.totalMessages++;
         }
       });
     }
@@ -60,17 +74,31 @@ app.get('/api-lyrics/:req', (req, res) => {
         .then(response => {
           // Handle successful response
           const jsonData = response.data;
+          stats.apiLyrics.numRequests++;
           res.json(jsonData);
         })
         .catch(error => {
           // Handle errors
           if (error.response.status == 404) {
             res.json({"Response": "No lyrics found, sorry buddy :("});
+            stats.apiLyrics.num404requests++;
             return;
           };
           console.error('Error fetching data:', error);
           res.send("504 Server Error");
     });
+});
+
+app.get('/stats', (req, res) => {
+  res.send(`
+    <h1>api.textyl.co</h1>
+    <p>Total Clients: ${stats.websocket.totalClients}</p>
+    <p>Total Messages: ${stats.websocket.totalMessages}</p>
+
+    <h1>WebSocket</h1>
+    <p>Number of Requests: ${stats.apiLyrics.numRequests}</p>
+    <p>Number of Not Found Lyrics: ${stats.apiLyrics.num404requests}</p>
+  `);
 });
 
 // Set the server to listen on a specific port
